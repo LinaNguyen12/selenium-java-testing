3f96cc664c4884cf581c93f4f1cf24bb94e541b2
Nga Nguyen <nguyenthinga12129494@gmail.com>
2025-05-14 13:56:25 +0700

    ẩn datatype

---
+++ selenium/JavaSDET/data_type.java
@@ -0,0 +1,9 @@
+package JavaSDET;
+
+public class data_type {
+    public static void main(String[] args) {
+        int i = 12;
+        if (i < 11) System.out.println(i);
+        else System.out.print(++i);
+    }
+}
