bfaac1b002e8bfb67c284f2826d6e90c90d37cf3
LinaNguyen12 <Nguyenthinga121254@gmail.com>
2025-05-14 15:13:55 +0700

    ẩn datatype 2

--- data_type.java
+++
@@ -1,9 +0,0 @@
-package JavaSDET;
-
-public class data_type {
-    public static void main(String[] args) {
-        int i = 12;
-        if (i < 11) System.out.println(i);
-        else System.out.print(++i);
-    }
-}
